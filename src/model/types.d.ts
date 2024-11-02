776aae2bf51d40dac8b94a9d8818c97955c68818
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2024-11-02 15:34:44 -0400

    Add studio and person types

--- src/model/types.d.ts
+++ src/model/types.d.ts
@@ -1,4 +1,16 @@
 declare global {
+  type Studio = {
+    mal_id: number;
+    type: string;
+    name: string;
+    url: string;
+  }
+
+  type Person = {
+    mal_id: number;
+    url: string;
+    name: string;
+  }
 }
 
 export {}
